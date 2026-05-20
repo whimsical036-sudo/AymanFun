00e7f82f796036acabe17d4cd6cca81d086495a1
whimsical036-sudo <whimsical036@gmail.com>
2026-05-20 17:41:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-// 
+// script.js
 
-let count = localStorage.getItem("clickCount") || 0;
+// Firebase Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-const countText = document.getElementById("clickCount");
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  get
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCfISHeNs_TflqRy8BACKWTinbkWAXwXgc",
+  authDomain: "counter-74305.firebaseapp.com",
+  projectId: "counter-74305",
+  storageBucket: "counter-74305.firebasestorage.app",
+  messagingSenderId: "464772006188",
+  appId: "1:464772006188:web:44edd41ec33d2d281d2c89",
+  measurementId: "G-E6V54XYKVR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// HTML Elements
 const button = document.getElementById("clickButton");
+const countText = document.getElementById("clickCount");
 
-countText.textContent = count;
+// Database Reference
+const countRef = ref(db, "counter");
 
-// ADDS TO COUNTER WHEN BUTTON IS CLICKED
-button.addEventListener("click", () => {
-  count++;
+// LIVE COUNTER UPDATES
+onValue(countRef, (snapshot) => {
+  const data = snapshot.val() || 0;
 
-  localStorage.setItem("clickCount", count);
+  countText.textContent = data;
+});
 
-  countText.textContent = count;
+// BUTTON CLICK
+button.addEventListener("click", async () => {
+
+  const snapshot = await get(countRef);
+
+  let current = snapshot.val() || 0;
+
+  current++;
+
+  set(countRef, current);
+
 });
